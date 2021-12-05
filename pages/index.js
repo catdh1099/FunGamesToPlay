80ea3a45df3e3321ba6ea2eca35f79b52b79b56e
catdh1099 <78674902+catdh1099@users.noreply.github.com>
2021-12-05 05:01:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+ <!DOCTYPE html>
+ <html>
+    <head>
+        <meta cnarset="utf-8">
+        <meta name="viewport" content="width=dwvice-width, initial-scale=1">
+        <title>Home</title>
+    </head>
+    <body>
+      <hi>Home</hi>
+     
+      <b>Website comeing soon!</b>
+      <hr>
+    </body>
+  </html>
+ 
+  
